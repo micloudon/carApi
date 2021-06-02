7ae88863b5cddbea5401d2ff0ecee463b74378cb
Michael Loudon <micloudon@gmail.com>
2021-06-02 10:56:05 -0700

    enabled CORS

--- djangoApi/settings.py
+++ djangoApi/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'restfulApi',
     'import_export',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'djangoApi.urls'
